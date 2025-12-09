285087895a03709434931e868814a181e9a3fc79
kayna <kaynanoliveira357@gmail.com>
2025-12-09 19:05:36 -0300

    commit

---
+++ src/app/_components/services.tsx
@@ -0,0 +1,11 @@
+export function Services() {
+  return (
+    <section className="bg-white py-16">
+      <div className="container mx-auto px-4">
+        <h2 className="text-4xl font-bold mb-12">Serviços</h2>
+
+        <div className="relative"></div>
+      </div>
+    </section>
+  );
+}
